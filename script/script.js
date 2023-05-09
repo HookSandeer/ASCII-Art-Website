5956e8ab34d6e112bec91f947af205198ad94db5
HookSander <66901837+HookSandeer@users.noreply.github.com>
2023-05-09 20:44:20 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 // By HookSander || Javascript
 
+const buttonBack = document.getElementById("back");
 const openGithubButton = document.getElementById("githubButton");
 const downloadCodeSource = document.getElementById("codeSourceButton");
 const downloadButton = document.getElementById("downloadButton");
@@ -14,4 +15,8 @@ downloadCodeSource.addEventListener("click", function() {
 
 downloadButton.addEventListener("click", function() {
     window.open("https://github.com/HookSandeer/ASCII-Art/releases/download/v4.0/Ascii_Art_Generator.exe");
+});
+
+buttonBack.addEventListener("click", function() {
+    window.location.href = "main.html";
 });

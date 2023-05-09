63598d7398937d71f661159f8f752471dd346871
HookSandeer <hooksander@proton.me>
2023-05-09 17:27:05 +0200

    Create Website ''

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// JS
+
+const buttonDownloadApp = document.getElementById("download");
+const buttonDowloadCodeSource = document.getElementById("downloadCodeSource")
+
+buttonDownloadApp.addEventListener("click", function() {
+    window.open("https://github.com/HookSandeer/ASCII-Art/archive/refs/heads/main.zip")
+})
+
+buttonDowloadCodeSource.addEventListener("click", function() {
+    window.open("")
+})
+
